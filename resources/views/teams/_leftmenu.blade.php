35aad348681466434ebe10f6267cfb0c1ad645b8
Anoop Naik <anoopnaik2016@gmail.com>
2016-05-08 13:42:24 +0530

    schedule match wip

--- resources/views/teams/_leftmenu.blade.php
+++ resources/views/teams/_leftmenu.blade.php
@@ -3,13 +3,13 @@
 	<div class="row">
 	<div class="team_view">
 		{!! Helper::Images(!empty($image[1])?$image[1]:'',!empty($image[0])?$image[0]:'',array('height'=>100,'width'=>100) )!!}
-		<h1>{{ $team_name or 'Team'}}</h1>
+                <h1 id="left-menu-team-name" data-team-id="{{$team_id}}" data-schedule-type="{{$sport_schedule_type}}">{{ $team_name or 'Team'}}</h1>
         @if(in_array($team_id,$managing_team_ids))
         <a href="{{ url('/team/edit').'/'.$team_id }}" class="tvp_edit">
           <span class="fa fa-pencil" title="Edit"></span>
         </a>
         @endif
-        <div class="locations"><i class="fa fa-map-marker"></i>&nbsp;<span style="word-wrap: break-word;">{{ !empty($location)?$location:'Location' }}</span>&nbsp;&nbsp;<i class="fa fa-globe"></i>&nbsp;<span>{{ $sport or 'Sport'}}</span></div>
+        <div class="locations"><i class="fa fa-map-marker"></i>&nbsp;<span style="word-wrap: break-word;">{{ !empty($location)?$location:'Location' }}</span>&nbsp;&nbsp;<i class="fa fa-globe"></i>&nbsp;<span id="left-menu-sport-name" data-sport-id="{{$sport_id}}">{{ $sport or 'Sport'}}</span></div>
         <div class="more desc">{{ $description or 'Description' }}</div>
         <?php if (!$user_in_team && $player_available_in_team) { ?>
         <div class="sb_join_team_main">
@@ -23,6 +23,17 @@
                         <span id="follow_unfollow_team_span_{{$team_id}}"><i class="{{ !empty($follow_unfollow)?'fa fa-remove':'fa fa-check' }}"></i>{{ !empty($follow_unfollow)?'Unfollow':'Follow' }}</span>
                 </a>
         </div>
+        <?php 
+        // commenting schedule match because of unreliable js
+        /*
+        <div class="btn_schedule_match">
+                <a href="javascript:void(0);" onclick="SJ.TEAM.scheduleMatch();">
+                        <span><i class="fa fa-check"></i>Schedule Match</span>
+                </a>
+        </div>
+         * 
+         */
+        ?>
 	</div>
      
 	<ul class="nav sidemenu_nav leftmenu-icon" id="side-menu">
