38af0a87d96177c90b4778c1c8f0dfa5f2afb520
Anoop Naik <anoopnaik2016@gmail.com>
2016-05-08 10:55:37 +0530

    changed the email address

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => env('MAIL_ADDRESS', 'ravi.gade@sportsjun.com'), 'name' => env('MAIL_FROM', 'SportsJun')],
+    'from' => ['address' => env('MAIL_ADDRESS', 'no-reply@sportsjun.com'), 'name' => env('MAIL_FROM', 'SportsJun')],
 
     /*
     |--------------------------------------------------------------------------
