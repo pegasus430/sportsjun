972482c680c3d8defe3d496966c1ebf5e23f295e
Anoop Naik <anoopnaik2016@gmail.com>
2016-05-18 15:43:26 +0530

    share bug fixes - sins of sapplica

--- resources/views/scorecards/soccerscorecardview.blade.php
+++ resources/views/scorecards/soccerscorecardview.blade.php
@@ -88,7 +88,6 @@
             </div>
         </div>
     </div>
-	{!! Form::open(array('url' => 'match/insertSoccerScoreCard', 'method' => 'POST')) !!}
     
     <div class="container pull-up">        
     
@@ -239,7 +238,7 @@
 	 
 </div>
 
-
+</div>
 <script>
 //Send Approve
 function scoreCardStatus(status)
