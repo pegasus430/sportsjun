d199ebd99dcedb1eb896d07d0490baeb3dad946e
Sachin Agarwal <sachinkumaragarwal05@gmail.com>
2016-06-12 23:40:48 +0530

    Added State name and country name for city search.

--- app/Model/State.php
+++ app/Model/State.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class State extends Model
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function country()
+    {
+        return $this->belongsTo(Country::class, 'country_id', 'id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function cities()
+    {
+        return $this->hasMany(City::class, 'state_id', 'id');
+    }
 }
