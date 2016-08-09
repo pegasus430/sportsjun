647307120e1c0cb42803b8cb6e241019fbc4986e
michel yossa <michel yossa>
2016-08-09 03:02:06 +0100

    organization changes

---
+++ resources/views/organization/event_points.blade.php
@@ -0,0 +1,55 @@
+<div id="event_points_{{$lis->id}}" class="modal fade">
+		<div class="modal-dialog sj_modal sportsjun-forms">
+			<div class="modal-content">
+				<div class="alert alert-danger" id="div_failure1"></div>
+				<div class="alert alert-success" id="div_success1" style="display:none;"></div>
+				<div class="modal-body">
+
+				<div class='row'>
+					<div class='col-sm-12'>
+						<div class='table-responsive'>
+							<table class='table table-striped table-bordered'>
+								<thead>
+									<tr>
+										<th>
+										</th>
+											@foreach($orgInfoObj->groups as $og)
+												<th>
+													
+													<img src="{{url('/uploads/org/groups/logo/'.$og->logo)}}"
+														 class='img-responsive img-rounded img-center' height='60px' width='60px'>
+												</th>
+											@endforeach
+								   </tr>
+								</thead>
+								<tbody>
+										<tr>
+											<td>{{$lis->sport->sports_name}} </td>
+
+											@foreach($orgInfoObj->groups as $og)
+													<td class="text-center">{{$lis->getGroupPoints($lis->id,$og->id)}}</td>
+											@endforeach
+										</tr>
+
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+
+
+					
+					
+
+
+				</div>
+
+
+
+				<div class="modal-footer">					
+					<button type="button" class="button btn-secondary" data-dismiss="modal">Close</button>
+				</div>
+				
+			</div>
+		</div>
+	</div>
---
+++ resources/views/organization/overall_standing.blade.php
@@ -0,0 +1,85 @@
+<div id="overall_standing_{{$parent_tournament->id}}" class="modal fade">
+		<div class="modal-dialog sj_modal sportsjun-forms">
+			<div class="modal-content">
+				<div class="alert alert-danger" id="div_failure1"></div>
+				<div class="alert alert-success" id="div_success1" style="display:none;"></div>
+				<div class="modal-body">
+					<div class='row'>
+					<div class='col-sm-12'>
+						<div class='table-responsive'>
+							<table class='table table-striped table-bordered'>
+								<thead>
+									<tr>
+										<th>&nbsp;
+										</th>
+											@foreach($orgInfoObj->groups as $og)
+												<th>
+													<?php $og->total_points=0;?>
+													<img src="{{url('/uploads/org/groups/logo/'.$og->logo)}}"
+														 class='img-responsive img-rounded center-block' height='60px' width='60px'>
+												</th>
+											@endforeach
+								   </tr>
+								</thead>
+								<tbody>
+									
+									@foreach($parent_tournament->getGroupPoints as $groupPoints)
+										<tr>
+											<td>{{$groupPoints->sport->sports_name}} </td>
+
+											@foreach($orgInfoObj->groups as $og)
+												<?php 
+												
+												$og_sports=$parent_tournament->getEachGroupPoints($parent_tournament->id,$og->id, $groupPoints->sports_id);
+												$og->total_points+=$og_sports;
+												?>
+
+										<td class="text-center">{{$og_sports}}</td>
+											@endforeach
+											
+										</tr>	
+									@endforeach
+
+
+										<tr>
+												<th>Total</th>
+											@foreach($orgInfoObj->groups as $og)
+												<th class="text-center">
+													{{ $og->total_points}}
+													
+												</th>
+											@endforeach
+										</tr>								
+
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+					
+
+
+				</div>
+
+
+
+				<div class="modal-footer">	
+					<button class='button btn-primary' onclick="reloadGroupTeams()">Refresh</button>				
+					<button type="button" class="button btn-secondary" data-dismiss="modal">Close</button>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+
+
+	<script>	
+	function reloadGroupTeams(){
+			$.ajax({
+				url:base_url+'/reloadgroupteampoints',
+				success:function(){
+					window.location=window.location;
+				}
+				})
+	}
+	</script>
