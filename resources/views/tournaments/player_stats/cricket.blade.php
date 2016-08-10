ba31b09d9accf00dfc655ab08e5af9f355f29596
michel yossa <michel yossa>
2016-08-10 12:57:38 +0100

    add cricket player standing

---
+++ resources/views/tournaments/player_stats/cricket.blade.php
@@ -0,0 +1,58 @@
+@if (count($player_standing))
+    <h4><b>{{ config('constants.SOCCER_STATS.HOCKEY_STATISTICS')}}</b></h4>
+    <div class=" stats-table" id='teamStatsDiv'>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>PLAYER NAME</th>
+                <th>TEAM NAME</th>
+                <th>{{ config('constants.STATISTICS.MATCHES')}}</th>                
+                <th>{{ config('constants.SOCCER_STATS.YELLOW_CARDS')}}</th>
+                <th>{{ config('constants.SOCCER_STATS.RED_CARDS')}}</th>
+                <th>{{ config('constants.SOCCER_STATS.GOALS_SCORED')}}</th>
+<!--                <th>{{ config('constants.SOCCER_STATS.GOALS_SAVED')}}</th>
+                <th>{{ config('constants.SOCCER_STATS.GOALS_ASSIST')}}</th>
+                <th>{{ config('constants.SOCCER_STATS.GOALS_PENALTIES')}}</th>-->
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($player_standing as $statistic)  
+            <tr>
+                <td>
+
+                    @if($statistic->url!='')
+                                <!--<img class="fa fa-user fa-fw fa-2x" height="42" width="42" src="{{ url('/uploads/user_profile/'.$statistic->url) }}" onerror="this.onerror=null;this.src='{{ asset('/images/default-profile-pic.jpg') }}';">-->
+                                
+                         
+                                    {!! Helper::Images($statistic->url,'user_profile',array('class'=>'img-circle img-border img-responsive lazy','height'=>52,'width'=>52) )!!}
+                             
+                                
+                                @else
+                            <!--    <img  class="fa fa-user fa-fw fa-2x" height="42" width="42" src="{{ asset('/images/default-profile-pic.jpg') }}">-->
+                              
+                                    {!! Helper::Images('default-profile-pic.jpg','images',array('class'=>'img-circle img-border img-responsive lazy','height'=>52,'width'=>52) )!!}
+                                
+                    
+                    @endif
+
+                 {{$statistic->player_name}}</td>                
+                <td><a href='/editsportprofile/{{$statistic->team_id}}' class="text-primary">{{$statistic->player_name}}</a></td>                
+                <td><a href='/team/members/{{$statistic->team_id}}' class="text-primary">{{$statistic->team_name}}</a></td>                
+                <td>{{$statistic->matches}}</td>
+                <td>{{$statistic->yellow_cards}}</td>
+                <td>{{$statistic->red_cards}}</td>
+                <td>{{$statistic->goals}}</td>
+<!--                <td>{{$statistic->goals_saved}}</td>
+                <td>{{$statistic->goal_assist}}</td>
+                <td>{{$statistic->goal_penalties}}</td>-->
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    </div>
+    @else
+    
+    <div class="sj-alert sj-alert-info">
+                       {{ trans('message.sports.nostats')}}
+</div>
+    @endif
