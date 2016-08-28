587253fd5eb4145f68afa3f2b585096f45096118
Anoop Naik <anoopnaik2016@gmail.com>
2016-08-28 21:13:06 +0530

    fixing database.php

--- config/database.php
+++ config/database.php
@@ -54,13 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => 'localhost',
-            'database'  => 'sportsjun_dev',
-            'username'  => 'sportsjun_dev',
-            'password'  => '8rAf@2af$dAw',
-            // 'database'  => 'sportsjun_dev',
-            // 'username'  => 'root',
-            // 'password'  => '',
+            'host'      => env('DB_HOST'),
+            'database'  => env('DB_DATABASE'),
+            'username'  => env('DB_USERNAME'),
+            'password'  => env('DB_PASSWORD'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
