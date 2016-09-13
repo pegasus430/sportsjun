0cabe6a2305c41365ad15701179f06b9a50ba4a8
Yossa Michel <ngambmicheal@gmail.com>
2016-09-13 16:29:20 +0100

    update editing and creating tournaments

--- resources/views/tournaments/_form.blade.php
+++ resources/views/tournaments/_form.blade.php
@@ -66,7 +66,7 @@ if(!empty($matchScheduleCount) && $matchScheduleCount>0)
 
 
     <div class="col-sm-6">
-        <div class="section show_hide_div"  style="display:none;">
+        <div class="section show_hide_div show_hide_game_type"  style="display:none;">
         <label class="form_label">{{  trans('message.tournament.fields.game_type') }} <span  class='required'>*</span></label>
             <label class="field select">
              {!! Form::select('game_type', $game_type_enum, null,array('id'=>'game_type','class'=>'gui-input',$disable)) !!}
@@ -83,7 +83,7 @@ if(!empty($matchScheduleCount) && $matchScheduleCount>0)
         <div class="section show_hide_rubber "  style="display:none;">
         <label class="form_label">{{  trans('message.tournament.fields.rubber_number') }} <span  class='required'>*</span></label>
             <label class="field select">
-             {!! Form::select('number_of_rubber', [3=>3, 5=>5,7=>7], 1,array('id'=>'rubber_number','class'=>'gui-input',$disable)) !!}
+             {!! Form::select('number_of_rubber', [3=>3, 5=>5,7=>7, 9=>9], 1,array('id'=>'rubber_number','class'=>'gui-input',$disable)) !!}
             
              <i class="arrow double"></i>      
             </label>
@@ -403,7 +403,7 @@ $(function () {
 		selected_sport = selected_sport.toUpperCase();
 		buildmatchtypedivs(selected_sport);
 	}
-	function buildmatchtypedivs(selected_sport)
+	function buildmatchtypedivs(selected_sport, div_type)
     {
      //get match type and schedules
       $.get(base_url+"/schedule/getmatchandplayertypes",{'sport_name':selected_sport,'from_tournament':'no'},function(response,status){
@@ -426,6 +426,27 @@ $(function () {
         }
         $("#main_match_typee").html(match_type_html);
         $("#main_player_typee").html(player_type_html);
+      });      
+    }
+
+    function buildmatchtypedivsOnly(selected_sport)
+    {
+     //get match type and schedules
+      $.get(base_url+"/schedule/getmatchandplayertypes",{'sport_name':selected_sport,'from_tournament':'no'},function(response,status){
+        var match_type_html = "<option value=''>Select Match Type</option>";
+       
+        if(status == 'success')
+        {
+          if(response.matchTypes)
+          {
+             $.each(response.matchTypes, function(key, value) {
+              match_type_html += "<option value='"+key+"'>"+value+"</option>";
+             });
+          }
+         
+        }
+        $("#main_match_typee").html(match_type_html);
+       
       });      
     }
 	  //schedule type on chanage
@@ -479,6 +500,90 @@ $(function () {
                 $('.show_hide_rubber').hide();
                  $('#rubber_number').val(1);
             } 
+    })
+
+    $("#schedule_type").change(function(){
+            if($(this).val()=='individual'){
+                 $('#rubber_number').val(1);
+                 $('#game_type').val('normal');
+                 $('.show_hide_rubber').hide();
+                 $('.show_hide_game_type').hide()
+            }
+            else{
+                 $('#rubber_number').val(3);
+                 $('#game_type').val('normal');
+                 $('.show_hide_show').hide();
+                 $('.show_hide_game_type').show()
+            }
+    })
+
+    $("#main_player_typee").change(function(){
+            if($(this).val()=='mixed'){
+                 $('#main_match_typee').val('doubles');
+                 $('#main_match_typee').attr('readonly', true);                 
+            }
+            else{              
+               $('#main_match_typee').removeAttr('readonly');
+
+            }
+    })
+
+    $("#main_player_typee").change(function(){
+    
+      if($(this).val()==='mixed')
+      {
+          // $('#main_myteam').attr('readonly', true);
+          $("#main_match_typee option[value='singles']").remove();         
+          $("#main_match_typee option[value='any']").remove();
+      }
+      else
+      {
+        var selected_sport = $("#sports_id option:selected").text();
+        selected_sport = selected_sport.toUpperCase();
+        buildmatchtypedivsOnly(selected_sport);
+      }
+    });
+
+
+    $(document).ready(function(){
+        var sport_name = $( "#sports_id option:selected" ).text();
+        if(sport_name.toLowerCase()=='tennis' || sport_name.toLowerCase()=='table tennis' || sport_name.toLowerCase()=='badminton' || sport_name.toLowerCase()=='squash')
+        {
+            $('#show_hide_div').show();
+            $('.show_hide_div').show();
+            $("#schedule_type").val('team');
+        }else
+        {
+            $('#show_hide_div').hide();
+            $('.show_hide_div').hide();
+            $('.show_hide_rubber').hide();
+        }
+
+
+
+            // if($("#schedule_type").val()=='individual'){
+            //      $('#rubber_number').val(1);
+            //      $('#game_type').val('normal');
+            //      $('.show_hide_rubber').hide();
+            //      $('.show_hide_game_type').hide()
+            // }
+            // else{
+            //      $('#rubber_number').val(3);
+            //      $('#game_type').val('normal');
+            //      $('.show_hide_show').hide();
+            //      $('.show_hide_game_type').show()
+            // }
+
+
+             if($('#game_type').val()=='rubber'){
+                $('.show_hide_rubber').show();
+                
+            }
+            else{
+                $('.show_hide_rubber').hide();
+               
+            } 
+   
     })
 
 
