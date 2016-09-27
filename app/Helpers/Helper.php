61039a568c235d8b4168735d1f8927fb40a9fe0e
Snow <snowcanbe@gmail.com>
2016-09-27 13:45:22 +0700

    fix update organization gallery images

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -44,14 +44,14 @@ class Helper {
         // Resize the files
         // Upload to specified folder (File storage or CDN)
         $photos = array_filter(explode(',', $photoList));
-        $oldFilePath = 'uploads/temp/';
-        $newFilePath = 'uploads/'.$photoType.'/';
+        $oldFilePath = public_path('uploads/temp/');
+        $newFilePath = public_path('uploads/'.$photoType.'/');
         if($action!='' && $action_id!='')//changed for gallery page
         {
-            $newFilePath = public_path().'/uploads/'.$photoType.'/'.$image_type.'/'.$action_id.'/';
-            if (!file_exists($newFilePath)) {
-                File::makeDirectory($newFilePath, $mode = 0777, true, true);
-            }
+            $newFilePath = $newFilePath.'/'.$image_type.'/'.$action_id.'/';
+        }
+        if (!file_exists($newFilePath)) {
+            File::makeDirectory($newFilePath, $mode = 0777, true, true);
         }
         $insertedphotoids=array();
         // echo"<pre>";print_r($photos);
