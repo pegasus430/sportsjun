2b525e9f84658d64733a1eeb9adb8f61e0845090
Snow <snowcanbe@gmail.com>
2016-10-08 17:40:04 +0700

    Update players tab data

--- resources/views/organization/members/partials/member_list.blade.php
+++ resources/views/organization/members/partials/member_list.blade.php
@@ -1,8 +1,9 @@
 <table class="table table-bordered">
-    <thead>
+    <thead class="member-user-thead">
     <tr>
         <th style="width:30%">Name</th>
         <th>Teams</th>
+        <th>Sports</th>
         <th>Stats/Notes</th>
     </tr>
     </thead>
@@ -11,7 +12,7 @@
         <tr>
             <td>
                 <a href="{{route('showsportprofile',['id'=>$member['id']])}}" class="member-image">
-                    {!! Helper::Images( (count($member['user']['photos'])?$member['user']['photos'][0]['url']:''),'user_profile',array('height'=>60 ) ) !!}
+                    {!! Helper::Images( (count($member['user']['photos'])?$member['user']['photos'][0]['url']:''),'user_profile',array('height'=>60,'width'=>60,'class'=>'img-circle img-border ') ) !!}
                 </a>
                 <span class="member-user-info">
                         <a href="{{route('showsportprofile',['id'=>$member['id']])}}">{{ $member->name }}</a>
@@ -20,11 +21,14 @@
             </td>
             <td>
                 @foreach ($member->userdetails as $teamPlayer)
-                    <a href="{{route('team/members',['team_id'=>object_get($teamPlayer,'team.id')])}}">{{object_get($teamPlayer,'team.name')}}</a>
+                        {{object_get($teamPlayer,'team.name')}}@if ($teamPlayer != $member->userdetails->last()), @endif
                 @endforeach
-
             </td>
-
+            <td>
+                @foreach ($member->userdetails as $teamPlayer)
+                    {{object_get($teamPlayer,'team.sports.sports_name')}}@if ($teamPlayer != $member->userdetails->last()), @endif
+                @endforeach
+            </td>
             <td></td>
         </tr>
     @endforeach
