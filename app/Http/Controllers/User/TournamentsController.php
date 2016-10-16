05f20426482d25b75d309fc61516adb42e60a6c1
Yossa Michel <ngambmicheal@gmail.com>
2016-10-16 15:43:37 +0100

    player standing kabaddi

--- app/Http/Controllers/User/TournamentsController.php
+++ app/Http/Controllers/User/TournamentsController.php
@@ -42,6 +42,7 @@ use App\Model\HockeyPlayerMatchwiseStats;
 use App\Model\BasketballPlayerMatchwiseStats;
 use App\Model\CricketPlayerMatchwiseStats;
 use App\Model\WaterpoloPlayerMatchwiseStats;
+use App\Model\KabaddiPlayerMatchwiseStats;
 
 
 class TournamentsController extends Controller
@@ -2495,6 +2496,26 @@ class TournamentsController extends Controller
 
 
 				break;
+
+					case 14; //kabaddi
+						$player=KabaddiPlayerMatchwiseStats::join('match_schedules', 'match_schedules.id', '=', 'kabaddi_player_matchwise_stats.match_id')
+							->join('teams', 'teams.id','=', 'kabaddi_player_matchwise_stats.team_id')
+							->join('users', 'users.id', '=', 'kabaddi_player_matchwise_stats.user_id')
+							->where('match_schedules.tournament_id', $tournament_id)
+							->select('kabaddi_player_matchwise_stats.*','users.*')							
+							->selectRaw('sum(points_1) as points_1')
+							->selectRaw('count(match_schedules.id) as matches')
+							->selectRaw('sum(points_2) as points_2')
+							->selectRaw('sum(points_3) as points_3')
+							->selectRaw('sum(total_points) as total_points')
+							->selectRaw('sum(fouls) as fouls')
+							->orderBy('total_points', 'desc')
+							->groupBy('user_id')
+							->get();
+
+
+				break;
+
 
 				case 16:	//water polo
 						$player=WaterpoloPlayerMatchwiseStats::join('match_schedules', 'match_schedules.id', '=', 'waterpolo_player_matchwise_stats.match_id')
