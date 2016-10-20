88d2a30f7e847c635988a558b82fa5889509917f
Snow <snowcanbe@gmail.com>
2016-10-20 07:06:05 +0700

    add email on team change ownership fix sending email, if registering was done

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 use App\Events\OrganizationStaffWasAdded;
+use App\Events\TeamOwnershipChanged;
+use App\Events\UserRegistered;
 use App\Handlers\Events\Staff\SendEmailToOrganizationStaff;
+use App\Listeners\EmailUserRegister;
+use App\Listeners\EmailUserTeamOwnership;
 use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
@@ -23,14 +27,17 @@ class EventServiceProvider extends ServiceProvider
         ],
 
         OrganizationStaffWasAdded::class => [
-          SendEmailToOrganizationStaff::class
+            SendEmailToOrganizationStaff::class
         ],
+        UserRegistered::class => [EmailUserRegister::class],
+        TeamOwnershipChanged::class => [EmailUserTeamOwnership::class]
+
     ];
 
     /**
      * Register any other events for your application.
      *
-     * @param  \Illuminate\Contracts\Events\Dispatcher  $events
+     * @param  \Illuminate\Contracts\Events\Dispatcher $events
      * @return void
      */
     public function boot(DispatcherContract $events)
