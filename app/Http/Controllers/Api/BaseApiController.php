932a63695cf8c1e3f16265883d31f11fad2843f4
Snow <snowcanbe@gmail.com>
2016-10-25 15:39:58 +0700

    add map response methods

--- app/Http/Controllers/Api/BaseApiController.php
+++ app/Http/Controllers/Api/BaseApiController.php
@@ -3,61 +3,100 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
 
 
 class BaseApiController extends Controller
 {
-    function applyFilter($query,$fields =[]){
+    function applyFilter($query, $fields = [])
+    {
         $data = \Request::all();
-        foreach ($fields as $field){
-            if (isset($data[$field])){
-                $query->where($field,$data[$field]);
+        foreach ($fields as $field) {
+            if (isset($data[$field])) {
+                $query->where($field, $data[$field]);
             }
         }
         return $query;
     }
 
 
-    function ApiResponse($response,$code = 200){
+    function ApiResponse($response, $code = 200)
+    {
         if (!$response && !is_array($response)) {
             $code = 404;
             $response = 'Not found';
         }
 
-        if ($code>=200 && $code<400)
+        if ($code >= 200 && $code < 400) {
             $status = 'Success';
-        else
+        } else {
             $status = 'Failure';
+        }
 
-        if ($response instanceof LengthAwarePaginator)
+        if ($response instanceof LengthAwarePaginator) {
             $response = $response->toArray();
+        }
 
 
         return [
-            'status'=>$status,
-            'code'=>$code,
-            'response'=>$response,
+            'status' => $status,
+            'code' => $code,
+            'response' => $response,
         ];
     }
 
-    function PaginatedMapResponse(LengthAwarePaginator $paginator,$map){
+    function PaginatedMapResponse(LengthAwarePaginator $paginator, $map)
+    {
         $data = [];
         foreach ($paginator as $item) {
-            $item_data =[];
-            foreach ($map as $key => $mapped){
-                if (!is_integer($key))
-                    $item_data[$key]= object_get($item,$mapped);
-                else
-                    $item_data[$mapped] = object_get($item,$mapped);
+            $item_data = [];
+            foreach ($map as $key => $mapped) {
+                if (!is_integer($key)) {
+                    $item_data[$key] = object_get($item, $mapped);
+                } else {
+                    $item_data[$mapped] = object_get($item, $mapped);
+                }
             }
-            $data[]=$item_data;
+            $data[] = $item_data;
         }
 
         $result = new LengthAwarePaginator($data, $paginator->total(), $paginator->perPage(),
             $paginator->currentPage(), ['path' => \Request::url(), 'query' => \Request::query()]);
 
         return $this->ApiResponse($result);
+    }
+
+    function CollectionMapResponse(Collection $collection,$map){
+        $data = [];
+        foreach ($collection as $item) {
+            $item_data = [];
+            foreach ($map as $key => $mapped) {
+                if (!is_integer($key)) {
+                    $item_data[$key] = object_get($item, $mapped);
+                } else {
+                    $item_data[$mapped] = object_get($item, $mapped);
+                }
+            }
+            $data[] = $item_data;
+        }
+
+        return $this->ApiResponse($data);
+    }
+
+    function ModelMapResponse(Model $model, $map)
+    {
+        $data = [];
+        foreach ($map as $key => $mapped) {
+            if (!is_integer($key)) {
+                $data[$key] = object_get($model, $mapped);
+            } else {
+                $data[$mapped] = object_get($model, $mapped);
+            }
+        }
+
+        return $this->ApiResponse($data);
     }
 
 
