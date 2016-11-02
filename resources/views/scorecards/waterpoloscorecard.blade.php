b26bcd9c7f7d212b7b9154ffed9522ed1d279e7d
Yossa Michel <ngambmicheal@gmail.com>
2016-11-02 02:43:24 +0100

    show owner/captain/keeper/vic captain on loading team players

--- resources/views/scorecards/waterpoloscorecard.blade.php
+++ resources/views/scorecards/waterpoloscorecard.blade.php
@@ -300,6 +300,7 @@ input:read-only {
 											<tr class="team_a_playing_row playing_a_{{$player_a['id']}}" >
 												<td class="option block select_player_squad" player_type='playing' team_type="team_a"  player_id="{{$player_a['id']}}">
 													{{ $player_a['name']   }}
+						{!!ScoreCard::display_role($player_a['id'], $team_a_id)!!}
 													<span class='pull-right icon-check'>   </span>
 												</td>
 											</tr>
@@ -325,6 +326,7 @@ input:read-only {
 												<td class="option block select_player_squad" player_type='playing' team_type="team_b" player_id="{{$player_b['id']}}">
 
 													{{ $player_b['name']   }}
+									{!!ScoreCard::display_role($player_b['id'], $team_b_id)!!}
 													<span class="pull-right icon-check"> </span>
 
 												</td>
@@ -357,6 +359,7 @@ input:read-only {
 												<td class="option block select_player_squad" player_type='substitute' team_type="team_a" player_id="{{$player_a['id']}}">
 
 													{{ $player_a['name']   }}
+								{!!ScoreCard::display_role($player_a['id'], $team_a_id)!!}
 													<span class='pull-right icon-check'></span>
 											</tr>
 										@endforeach
@@ -383,6 +386,7 @@ input:read-only {
 												<td class="option block select_player_squad" player_type='substitute' team_type="team_b" player_id="{{$player_b['id']}}">
 
 													{{ $player_b['name']   }}
+							{!!ScoreCard::display_role($player_b['id'], $team_b_id)!!}
 													<span class="pull-right icon-check"> </span>
 												</td>
 											</tr>
