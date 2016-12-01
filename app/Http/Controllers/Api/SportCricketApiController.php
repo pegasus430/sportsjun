ab54534cf6d26bbd867523accc06772ff8d0ff46
Snow <snowcanbe@gmail.com>
2016-12-01 15:06:33 +0700

    update owerwise score post

--- app/Http/Controllers/Api/SportCricketApiController.php
+++ app/Http/Controllers/Api/SportCricketApiController.php
@@ -13,53 +13,57 @@ class  SportCricketApiController extends BaseApiController
     public function cricketStatistics()
     {
         $statistic = $this
-                        ->applyFilter(CricketStatistic::select(),[])
-                        ->paginate(50);
+            ->applyFilter(CricketStatistic::select(), [])
+            ->paginate(50);
 
         return $this->ApiResponse($statistic);
     }
 
-    public function cricketPlayerMatchScore(){
+    public function cricketPlayerMatchScore()
+    {
         $stats = $this
-                         ->applyFilter(CricketPlayerMatchwiseStats::select(),[])
-                         ->paginate(50);
+            ->applyFilter(CricketPlayerMatchwiseStats::select(), [])
+            ->paginate(50);
         return $this->ApiResponse($stats);
     }
 
-    public function cricketOverwiseScore(){
-        $score =  $this
-                         ->applyFilter(CricketOverwiseScore::select(),[])
-                         ->paginate(50);
+    public function cricketOverwiseScore()
+    {
+        $score = $this
+            ->applyFilter(CricketOverwiseScore::select(), [])
+            ->paginate(50);
         return $this->ApiResponse($score);
     }
 
-    public function setCricketOverwiseScore($id){
+    public function setCricketOverwiseScore($id = false)
+    {
         $data = \Request::all();
 
         $validator = \Validator::make($data, [
             'tournament_id' => 'required|exists:tournaments,id',
             'team_id' => 'required|exists:teams,id',
             'match_id' => 'required|exists:match_schedules,id',
-            'over'=>'required',
-            'ball_by_ball' => 'required'
+            'over' => 'required|numeric',
+            'bowler_id' => 'required|numeric',
+            'ball_by_ball' => ''
         ]);
 
+
         if (!$validator->fails()) {
-            $user = \Auth::user();
-            if ($id){
-                // can edit ?
-                $error = 'Editing not allowed';
-            } else {
-                $score = new CricketOverwiseScore();
-                $score->user_id = $user->id;
-                $score->tournament_id = $data['tournament_id'];
-                $score->team_id = $data['team_id'];
-                $score->over = $data['over'];
+            $score = CricketOverwiseScore::firstOrCreate([
+                'tournamet_id' => $data['tournament_id'],
+                'team_id' => $data['team_id'],
+                'over' => $data['over'],
+                'bowler_id' => $data['bowler_id'],
+            ]);
+            if ($data['ball_by_ball']) {
                 $score->ball_by_ball = $data['ball_by_ball'];
                 $score->isactive = 1;
-                $score->save();
-                return $this->ApiResponse($score->id);
+
             }
+            $score->save();
+            return $this->ApiResponse(['message'=>'ok']);
+
         } else {
             $error = $validator->errors()->first();
         }
