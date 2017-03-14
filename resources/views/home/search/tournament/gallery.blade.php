ab623d989c56ce1f76250dba94e5e84bcc093556
Snow <snowcanbe@gmail.com>
2017-03-14 16:20:50 +0700

    update tournaments gallery search results

--- resources/views/home/search/tournament/gallery.blade.php
+++ resources/views/home/search/tournament/gallery.blade.php
@@ -10,24 +10,28 @@
                 <a href="#form_gallery">
                     <img src="{{ Helper::getImagePath($form_gallery_photo->url,'tournaments') }}"
                          class="img img-responsive"/>
+                    Form Gallery ({{$form_gallery_count}})
                 </a>
             </div>
         @endif
 
-        @foreach ($tournament->albums as $album)
-            <?php
-            $photo = $album->photos->first();
-            ?>
-            @if ($photo)
-                <div class="col-md-3 div-col-sm-4 col-xs-12">
-                    <a href="#form_gallery">
-
-                        <img src="{{ Helper::getImagePath($photo->url,'tournaments') }}"
-                             class="img img-responsive"/>
-                    </a>
-                </div>
-            @endif
-        @endforeach
+        @if ($tournament->albums && $tournament->albums->count())
+            <h2>Albums</h2>
+            @foreach ($tournament->albums as $album)
+                <?php
+                $photo = $album->photos->first();
+                ?>
+                @if ($photo)
+                    <div class="col-md-3 div-col-sm-4 col-xs-12">
+                        <a href="#form_gallery">
+                            <img src="{{ Helper::getImagePath($photo->url,'tournaments') }}"
+                                 class="img img-responsive"/>
+                            ({{ $album->photos->count() }})
+                        </a>
+                    </div>
+                @endif
+            @endforeach
+        @endif
     </div>
 
 </div>
