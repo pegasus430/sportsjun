11951ecce58602fbc64ce39409dad9ce7d298667
Snow <snowcanbe@gmail.com>
2017-04-14 19:03:21 +0700

    update model

--- app/Model/BasketballStatistic.php
+++ app/Model/BasketballStatistic.php
@@ -8,4 +8,45 @@ class BasketballStatistic extends Model
 {
     //
      protected $table = 'basketball_statistic';
+
+    public static function updateUserStatistic($user_id){
+        //check already player has record or not
+        $user_basketball_details = BasketballStatistic::select()->where('user_id',$user_id)->get();
+
+        $basketball_details = BasketballPlayerMatchwiseStats::selectRaw('count(match_id) as match_count')->selectRaw('sum(points_1) as points_1')->selectRaw('sum(points_2) as points_2')->selectRaw('sum(points_3) as points_3')->selectRaw('sum(total_points) as total_points')->selectRaw('sum(fouls) as fouls')->where('user_id',$user_id)->groupBy('user_id')->get();
+
+
+
+        $points_1 = (!empty($basketball_details[0]['points_1']))?$basketball_details[0]['points_1']:0;
+        $points_2 = (!empty($basketball_details[0]['points_2']))?$basketball_details[0]['points_2']:0;
+        $points_3 = (!empty($basketball_details[0]['points_3']))?$basketball_details[0]['points_3']:0;
+        $fouls = (!empty($basketball_details[0]['fouls']))?$basketball_details[0]['fouls']:0;
+        $total_points = (!empty($basketball_details[0]['total_points']))?$basketball_details[0]['total_points']:0;
+
+        if(count($user_basketball_details)>0)
+        {
+            $match_count = (!empty($basketball_details[0]['match_count']))?$basketball_details[0]['match_count']:0;
+
+            BasketballStatistic::where('user_id',$user_id)
+                               ->update([	'matches'=>$match_count,
+                                             'points_1'=>$points_1,
+                                             'points_2'=>$points_2,
+                                             'points_3'=>$points_3,
+                                             'fouls'=>$fouls,
+                                             'total_points'=>$total_points
+                               ]);
+        }else
+        {
+            $basketball_statistic = new BasketballStatistic();
+            $basketball_statistic->user_id = $user_id;
+            $basketball_statistic->matches = 1;
+            $basketball_statistic->{'points_1'} = $points_1;
+            $basketball_statistic->{'points_2'} = $points_2;
+            $basketball_statistic->{'points_3'} = $points_3;
+            $basketball_statistic->fouls = $fouls;
+            $basketball_statistic->total_points = $total_points;
+            $basketball_statistic->save();
+        }
+    }
+
 }
--- app/Model/HockeyStatistic.php
+++ app/Model/HockeyStatistic.php
@@ -12,4 +12,28 @@ class HockeyStatistic extends Model {
     protected $table = 'hockey_statistic';
     //protected $fillable = array('user_id', 'following_sports', 'following_teams', 'managing_teams', 'joined_teams', 'following_tournaments', 'managing_tournaments', 'joined_tournaments', 'following_players', 'following_facilities', 'provider_id', 'provider_token', 'avatar',);
 
+    public static function updateUserStatistic($user_id){
+        //check already player has record or not
+        $user_hockey_details = HockeyStatistic::select()->where('user_id',$user_id)->get();
+
+        $hockey_details = HockeyPlayerMatchwiseStats::selectRaw('count(match_id) as match_count')->selectRaw('sum(yellow_cards) as yellow_cards')->selectRaw('sum(red_cards) as red_cards')->selectRaw('sum(goals_scored) as goals_scored')->where('user_id',$user_id)->groupBy('user_id')->get();
+        $yellow_card_cnt = (!empty($hockey_details[0]['yellow_cards']))?$hockey_details[0]['yellow_cards']:0;
+        $red_card_cnt = (!empty($hockey_details[0]['red_cards']))?$hockey_details[0]['red_cards']:0;
+        $goals_cnt = (!empty($hockey_details[0]['goals_scored']))?$hockey_details[0]['goals_scored']:0;
+        if(count($user_hockey_details)>0)
+        {
+            $match_count = (!empty($hockey_details[0]['match_count']))?$hockey_details[0]['match_count']:0;
+            HockeyStatistic::where('user_id',$user_id)->update(['matches'=>$match_count,'yellow_cards'=>$yellow_card_cnt,'red_cards'=>$red_card_cnt,'goals_scored'=>$goals_cnt]);
+        }else
+        {
+            $hockey_statistics = new HockeyStatistic();
+            $hockey_statistics->user_id = $user_id;
+            $hockey_statistics->matches = 1;
+            $hockey_statistics->yellow_cards = $yellow_card_cnt;
+            $hockey_statistics->red_cards = $red_card_cnt;
+            $hockey_statistics->goals_scored = $goals_cnt;
+            $hockey_statistics->save();
+        }
+    }
+
 }
