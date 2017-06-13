e2a98605ed1d184fcbef04472690eed74f226d4b
Yossa Michel <ngambmicheal@gmail.com>
2017-06-13 08:12:56 +0100

    fixed migrations

--- database/migrations/2017_04_03_102344_UpdateMatchSchedulesTable.php
+++ database/migrations/2017_04_03_102344_UpdateMatchSchedulesTable.php
@@ -12,13 +12,13 @@ class UpdateMatchSchedulesTable extends Migration
      */
     public function up()
     {
-        Schema::table('match_schedules', function (Blueprint $table) {
-            $table->text('a_playing_players')->nullable();
-            $table->text('b_playing_players')->nullable();
+        // Schema::table('match_schedules', function (Blueprint $table) {
+        //     $table->text('a_playing_players')->nullable();
+        //     $table->text('b_playing_players')->nullable();
 
-            $table->string('a_sub',255)->nullable();
-            $table->string('b_sub',255)->nullable();
-        });
+        //     $table->string('a_sub',255)->nullable();
+        //     $table->string('b_sub',255)->nullable();
+        // });
     }
 
     /**
@@ -28,11 +28,11 @@ class UpdateMatchSchedulesTable extends Migration
      */
     public function down()
     {
-        Schema::table('match_schedules', function (Blueprint $table) {
-            $table->dropColumn('a_playing_players');
-            $table->dropColumn('b_playing_players');
-            $table->dropColumn('a_sub');
-            $table->dropColumn('b_sub');
-        });
+        // Schema::table('match_schedules', function (Blueprint $table) {
+        //     $table->dropColumn('a_playing_players');
+        //     $table->dropColumn('b_playing_players');
+        //     $table->dropColumn('a_sub');
+        //     $table->dropColumn('b_sub');
+        // });
     }
 }
